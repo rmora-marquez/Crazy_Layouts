b7659f004af06cde52d8ed0e70296d1002eb685a
Ramon Mora Marquez <60113219+rmora-marquez@users.noreply.github.com>
2021-10-25 18:15:22 -0500

    ejemplo linear layout

--- app/src/main/java/edu/ieu/crazylayouts/MainActivity.java
+++ app/src/main/java/edu/ieu/crazylayouts/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.actvidad_principal);
+        setContentView(R.layout.ejemplo_linear_layout);
     }
 }
